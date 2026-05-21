851493c64b60aa75411b183011ff6139863df370
Ian Augusto Figueroa <figueroa.ian13@gmail.com>
2026-05-21 19:17:10 -0300

    Completo clase CasoList y ejemplos correspondientes.

--- Dsw2026Ej11/Tests/Ejemplos.cs
+++ Dsw2026Ej11/Tests/Ejemplos.cs
@@ -1,4 +1,7 @@
-﻿namespace Dsw2026Ej11.Tests;
+﻿using Dsw2026Ej11.Collections;
+using Dsw2026Ej11.Domain;
+
+namespace Dsw2026Ej11.Tests;
 
 
 internal class Ejemplos
@@ -11,7 +14,47 @@ internal class Ejemplos
     //Eliminar el primer elemento de la lista y listar por consola los alumnos
     public static void EjemploList()
     {
+        Console.WriteLine("=== EJEMPLO LIST ===");
+        CasoList casoList = new CasoList();
 
+        Alumno a1 = new Alumno(1, "Ian Figueroa", 8);
+        Alumno a2 = new Alumno(2, "Enzo Perez", 6);
+        Alumno a3 = new Alumno(3, "Julian Alvarez", 10);
+
+        casoList.AgregarAlumno(a1);
+        casoList.AgregarAlumno(a2);
+        casoList.AgregarAlumno(a3);
+
+        Console.WriteLine("\n--- LISTAR ALUMNOS ---");
+
+        foreach(Alumno a in casoList.RetornarAlumno())
+        {
+            Console.WriteLine(a);
+        }
+
+        Console.WriteLine("\n--- BUSCANDO ALUMNO Ian Figueroa ---");
+
+        Alumno alumnoExistente = casoList.BuscarAlumno("Ian Figueroa");
+        Console.WriteLine(alumnoExistente != null ? alumnoExistente.ToString() : "Alumno buscado no existe");
+
+        Console.WriteLine("\n--- BUSCANDO ALUMNO Lionel Messi ---");
+
+        Alumno alumnoInexistente = casoList.BuscarAlumno("Lionel Messi");
+        Console.WriteLine(alumnoInexistente != null ? alumnoInexistente.ToString() : "Alumno buscado no existe");
+
+        Console.WriteLine("\n--- ELIMINAR ALUMNO Enzo Perez Y LISTAR ---");
+        casoList.EliminarAlumno(a2);
+        foreach (Alumno a in casoList.RetornarAlumno())
+        {
+            Console.WriteLine(a);
+        }
+
+        Console.WriteLine("\n--- ELIMINAR ALUMNO EN PRIMER POSICIÓN Y LISTAR ---");
+        casoList.EliminarAlumno(0);
+        foreach (Alumno a in casoList.RetornarAlumno())
+        {
+            Console.WriteLine(a);
+        }
     }
 
     //Agregar 3 alumnos al diccionario
